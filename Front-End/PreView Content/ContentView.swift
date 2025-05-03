01b4076535b932149d95076407376f38d43682f2
Jean-Parnell I Louis <jeanparnellone@gmail.com>
2025-05-03 18:22:57 -0400

    Update ContentView.swift

--- Front-End/PreView Content/ContentView.swift
+++ Front-End/PreView Content/ContentView.swift
@@ -1,1 +1,37 @@
+//
+//  ContentView.swift
+//  Front-End page
+//
+//  Created by Linh Yui on 5/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack{
+            Image("Starry Mountain")
+                .resizable()
+                .frame(width: 900, height: 2000)
+            
+            VStack{
+                Text("Clean Living")
+                    .bold()
+                    .italic()
+                    .font(.title)
+                    .foregroundColor(Color(hue: 0.811, saturation: 0.884, brightness: 0.703))
+                
+                Text("Click here ")
+            }
+            .showEnvironmentalMessages(
+                initialDelay: 10,
+                repeatInterval: 50...400
+            )
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
 
