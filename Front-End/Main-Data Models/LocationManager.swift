56e872cfef98bf78f94dd6b427704fb519234ed2
Jean-Parnell I Louis <jeanparnellone@gmail.com>
2025-05-03 18:21:41 -0400

    Update LocationManager.swift

--- Front-End/Main-Data Models/LocationManager.swift
+++ Front-End/Main-Data Models/LocationManager.swift
@@ -1,1 +1,74 @@
+//
+//  LocationManager.swift
+//  Front-End page
+//
+//  Created by Linh Yui on 4/30/25.
+//
 
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    private let manager = CLLocationManager()
+    
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    @Published var authorizationStatus: CLAuthorizationStatus = .notDetermined
+    @Published var lastError: Error?
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters // Balanced accuracy
+    }
+    
+    func requestLocation() {
+        guard authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse else {
+            requestAuthorization()
+            return
+        }
+        
+        isLoading = true
+        lastError = nil
+        manager.requestLocation()
+    }
+    
+    func requestAuthorization() {
+        isLoading = true
+        manager.requestWhenInUseAuthorization()
+    }
+    
+    // MARK: - CLLocationManagerDelegate
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+        self.location = location.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        lastError = error
+        isLoading = false
+        print("Location manager failed with error: \(error.localizedDescription)")
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+        
+        switch manager.authorizationStatus {
+        case .authorizedAlways, .authorizedWhenInUse:
+            manager.requestLocation()
+        case .denied, .restricted:
+            lastError = CLError(.denied)
+        case .notDetermined:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    // Helper to check if location services are enabled
+    var isLocationServicesEnabled: Bool {
+        CLLocationManager.locationServicesEnabled()
+    }
+}
