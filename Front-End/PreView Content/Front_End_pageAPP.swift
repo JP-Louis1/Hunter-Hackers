f8ba00e92c4afc9ed2f5237526b964822271ae2d
Jean-Parnell I Louis <jeanparnellone@gmail.com>
2025-05-03 18:23:33 -0400

    Update Front_End_pageAPP.swift

--- Front-End/PreView Content/Front_End_pageAPP.swift
+++ Front-End/PreView Content/Front_End_pageAPP.swift
@@ -1,1 +1,17 @@
+//
+//  Front_End_pageApp.swift
+//  Front-End page
+//
+//  Created by Linh Yui on 4/30/25.
+//
 
+import SwiftUI
+
+@main
+struct Front_End_pageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
