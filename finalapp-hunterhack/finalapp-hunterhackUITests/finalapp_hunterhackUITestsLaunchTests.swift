7ce545665fb2ba0ae074577e249681a098d35fdd
PhuongLinh Le <linhnhim2404@gmail.com>
2025-05-04 11:36:37 -0400

    Add files via upload

---
+++ finalapp-hunterhack/finalapp-hunterhackUITests/finalapp_hunterhackUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  finalapp_hunterhackUITestsLaunchTests.swift
+//  finalapp-hunterhackUITests
+//
+//  Created by Linh Yui on 5/4/25.
+//
+
+import XCTest
+
+final class finalapp_hunterhackUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
