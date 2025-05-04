0150cf2fbf4068c440f06cf2d382d9d5957be5b1
PhuongLinh Le <linhnhim2404@gmail.com>
2025-05-04 11:28:22 -0400

    Add files via upload

---
+++ finalapp-hunterhack/Preview Content/finalapp_hunterhackApp.swift
@@ -0,0 +1,17 @@
+//
+//  finalapp_hunterhackApp.swift
+//  finalapp-hunterhack
+//
+//  Created by Linh Yui on 5/4/25.
+//
+
+import SwiftUI
+
+@main
+struct finalapp_hunterhackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
