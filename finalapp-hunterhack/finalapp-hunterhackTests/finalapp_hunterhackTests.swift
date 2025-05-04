ec58e7fbb7d44de9bdd3ed5543b6cbf34e2e1011
PhuongLinh Le <linhnhim2404@gmail.com>
2025-05-04 11:35:17 -0400

    Update finalapp_hunterhackTests.swift

--- finalapp-hunterhack/finalapp-hunterhackTests/finalapp_hunterhackTests.swift
+++ finalapp-hunterhack/finalapp-hunterhackTests/finalapp_hunterhackTests.swift
@@ -1,1 +1,18 @@
+//
+//  finalapp_hunterhackTests.swift
+//  finalapp-hunterhackTests
+//
+//  Created by Linh Yui on 5/4/25.
+//
+
+import Testing
+@testable import finalapp_hunterhack
+
+struct finalapp_hunterhackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
 
